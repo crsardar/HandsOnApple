fc0a70e86ab99748021b097c4a5c16bcf4bdacb4
Chittaranjan Sardar <crsardar@gmail.com>
2019-06-29 09:34:04 +0530

    HandsOnApple: Structure

--- swift-playground/StructureHandson.playground/Contents.swift
+++ swift-playground/StructureHandson.playground/Contents.swift
@@ -1,3 +1,58 @@
 import UIKit
 
-var str = "Hello, playground"
+struct Person{
+    
+    var name: String
+    var age: Int
+}
+var aPerson = Person(name: "Chittaranjan", age: 31)
+print(aPerson)
+aPerson.name = "Shiva"
+print(aPerson)
+
+//
+struct People{
+    
+    var name: String
+    var age: Int
+    
+    mutating func setName(name:String){
+        
+        self.name = name
+    }
+}
+var aPeople = People(name: "Ohmkar", age: 9999999999)
+print(aPeople)
+aPeople.setName(name: "Adiyogi")
+print(aPeople)
+
+//
+struct Man{
+    
+    static var myStatic: Int = 111
+    var name: String
+    var age: Double
+    
+    init(name: String, ageYear: Double){
+        
+        self.name = name
+        age = ageYear
+    }
+    
+    init(name: String, ageMonth: Double){
+        
+        self.name = name
+        age = (ageMonth/12)
+    }
+    
+    func getStatic() -> Int{
+        
+        return Man.myStatic
+    }
+}
+var manOne = Man(name: "Agni", ageMonth: 49)
+var manTwo = Man(name: "Bayau", ageYear: 35)
+print(manOne)
+print("Man.myStatic = \(Man.myStatic)")
+print(manTwo)
+print("manTwo#myStatic = \(manTwo.getStatic())")
